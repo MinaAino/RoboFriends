e7654fe4a6ccedda09fe2a56ba5fe80da8f54310
MinaAino <68559887+MinaAino@users.noreply.github.com>
2020-08-14 18:31:43 -0700

    adding more

---
+++ src/robots.js
@@ -0,0 +1,46 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Leanne Grahan',
+	username: 'Bret',
+	email: 'Sincereapril.biz'
+},
+{
+	id: 2,
+	name: 'Ervin Howell',
+	username: 'Bret',
+	email: 'Shana@melissa.tv'
+},
+{
+	id: 3,
+	name: 'Clementine Bauch',
+	username: 'Bret',
+	email: 'Nathan@yeana.net'
+},
+{
+	id: 4,
+	name: 'Leanne Grahan',
+	username: 'Bret',
+	email: 'Sincereapril.biz'
+},
+{
+	id: 5,
+	name: 'Leanne Grahan',
+	username: 'Bret',
+	email: 'Sincereapril.biz'
+},
+{
+	id: 6,
+	name: 'Leanne Grahan',
+	username: 'Bret',
+	email: 'Sincereapril.biz'
+},
+{
+	id: 7,
+	name: 'Leanne Grahan',
+	username: 'Bret',
+	email: 'Sincereapril.biz'
+},
+];
+
+export default robots
